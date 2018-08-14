e81849c54810df4a698623d83fabba7a2c2fd7c0
mk-pmb <mk-pmb@users.noreply.github.com>
2018-08-14 18:39:22 +0200

    v0.1.0

---
+++ fatal.js
@@ -0,0 +1,8 @@
+﻿/*jslint indent: 2, maxlen: 80, continue: false, unparam: false, node: true */
+/* -*- tab-width: 2 -*- */
+'use strict';
+
+process.on('unhandledRejection', function rethrowSoon(e, p) {
+  e.promise = p;
+  setImmediate(function rethrowNow() { throw e; });
+});
