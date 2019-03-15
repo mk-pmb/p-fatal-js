77db58e5b9df50f42fe1921626465481f85fab99
mk-pmb <mk-pmb@users.noreply.github.com>
2019-03-15 17:59:57 +0100

    v0.1.2: Deal with thrown non-Errors

--- fatal.js
+++ fatal.js
@@ -3,6 +3,22 @@
 'use strict';
 
 process.on('unhandledRejection', function rethrowSoon(e, p) {
-  e.promise = p;
+  var o;
+  if (!e) {
+    o = e;
+    try {
+      e = '"' + String(e) + '"';
+    } catch (c) {
+      e = 'something that cannot even be stringified.';
+    }
+    e = new Error('unhandledRejection is not an object: ' + e);
+    e.origFalseyError = o;
+  }
+  try {
+    e.promise = p;
+  } catch (ignore) {
+    // We tried. Propagating this error would mask the original one,
+    // which is probably more important.
+  }
   setImmediate(function rethrowNow() { throw e; });
 });
